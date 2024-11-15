b9cf1ad3788893e79e43e6f53c5eeea85dcc0225
KabinsLuxuryLiving <Taylor@KabinsLuxuryLiving.com>
2024-11-15 16:00:56 -0500

    adding more gh pages configs

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-    domains: ['arktifact.com']
+    domains: ['arktifact.com', 'www.arktifact.com']
   },
   basePath: '',
   assetPrefix: '',
@@ -17,6 +17,14 @@ const nextConfig = {
             key: 'Strict-Transport-Security',
             value: 'max-age=31536000; includeSubDomains'
           },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          }
         ],
       },
     ]
