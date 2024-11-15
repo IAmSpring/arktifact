5ee0a8e2604316f4666cf05bd537fe1f97176783
KabinsLuxuryLiving <Taylor@KabinsLuxuryLiving.com>
2024-11-15 14:50:59 -0500

    build fix

--- data/season1/index.ts
+++ data/season1/index.ts
@@ -1,19 +1,22 @@
-import { episode1NFTs } from './episode1';
-import { episode2NFTs } from './episode2';
-import { episode3NFTs } from './episode3';
-import { episode4NFTs } from './episode4';
-import { episode5NFTs } from './episode5';
-import { episode6NFTs } from './episode6';
-import { episode7NFTs } from './episode7';
-import { episode8NFTs } from './episode8';
+import episode1NFTs from './episode1';
+import episode2NFTs from './episode2';
+import episode3NFTs from './episode3';
+import episode4NFTs from './episode4';
+import episode5NFTs from './episode5';
+import episode6NFTs from './episode6';
+import episode7NFTs from './episode7';
+import episode8NFTs from './episode8';
 
-export const season1NFTs = {
-  episode1: episode1NFTs,
-  episode2: episode2NFTs,
-  episode3: episode3NFTs,
-  episode4: episode4NFTs,
-  episode5: episode5NFTs,
-  episode6: episode6NFTs,
-  episode7: episode7NFTs,
-  episode8: episode8NFTs,
-}; 
+// Combine all episodes into a single array
+const allNFTs = [
+  ...episode1NFTs,
+  ...episode2NFTs,
+  ...episode3NFTs,
+  ...episode4NFTs,
+  ...episode5NFTs,
+  ...episode6NFTs,
+  ...episode7NFTs,
+  ...episode8NFTs
+];
+
+export default allNFTs; 
