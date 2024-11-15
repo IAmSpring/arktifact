0c734d752fa3ec46401f2ef6bcedc5d360104d17
KabinsLuxuryLiving <Taylor@KabinsLuxuryLiving.com>
2024-11-15 16:07:06 -0500

    latest fixes for build to prod

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     domains: ['arktifact.com', 'www.arktifact.com']
   },
   basePath: '',
-  assetPrefix: '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://arktifact.com' : '',
   trailingSlash: true,
   async headers() {
     return [
@@ -15,7 +15,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
+            value: 'max-age=31536000; includeSubDomains; preload'
           },
           {
             key: 'X-Frame-Options',
@@ -24,6 +24,10 @@ const nextConfig = {
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
           }
         ],
       },
